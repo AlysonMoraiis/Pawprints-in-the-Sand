9feffe23f74c00d45ac28ad4ec458ab06943d8aa
Alyson Morais <alysonmorais35@hotmail.com>
2023-07-23 13:35:38 -0300

    Adjusts on chase

--- Assets/Scripts/Enemies/AI/FlyNavgationAI.cs
+++ Assets/Scripts/Enemies/AI/FlyNavgationAI.cs
@@ -5,19 +5,46 @@ using UnityEngine.AI;
 
 public class FlyNavgationAI : MonoBehaviour
 {
+    [SerializeField] private float _detectDistance;
+    [SerializeField] private float _stopChaseDistance;
+
     private GameObject _player;
     private NavMeshAgent _agent;
 
+    private bool _canChase = false;
+
     void Start()
+    {
+        SetupAgent();
+    }
+
+    private void SetupAgent()
     {
         _agent = GetComponent<NavMeshAgent>();
         _agent.updateRotation = false;
         _agent.updateUpAxis = false;
-        _player = GameObject.Find("Player");
+        _player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        _agent.SetDestination(_player.transform.position);
+        CheckCanChase();
+    }
+
+    private void CheckCanChase()
+    {
+        float distance = Vector2.Distance(transform.position, _player.transform.position);
+
+        _canChase = (_detectDistance > distance);
+
+        if (_canChase)
+        {
+            _agent.SetDestination(_player.transform.position);
+
+            if (_stopChaseDistance > distance)
+            {
+                _canChase = false;
+            }
+        }
     }
 }
